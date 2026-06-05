38b8e2f8d51091f8ce0504d239ec85fd0a3b9764
mehran <mehranmohamadi1311@gmail.com>
2026-06-05 15:51:31 +0330

    docs(tailwindcss): install tailwindcss for project styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Initial setup</h1>;
+  return (
+    <h1 className="text-center pt-16 font-bold text-2xl text-indigo-500">
+      Initial setup
+    </h1>
+  );
 }
 
 export default App;
